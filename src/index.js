84daf7a2a31e272aa257a2184812db6c22cae6e1
bhushan <a230141@gmail.com>
2024-03-27 18:33:10 +0530

    going cookies

--- src/index.js
+++ src/index.js
@@ -4,3 +4,11 @@ dotenv.config({
     path: './env'
 })
 connectDB()
+    .then(() => {
+        app.listen(process.env.PORT || 8000, () => {
+            console.log(`server is running `);
+        })
+    })
+    .catch((err) => {
+        console.log("MDB COnnection failer", err);
+    })

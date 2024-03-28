fe339fc6414575d828630649c21553b434cee00e
bhushan <a230141@gmail.com>
2024-03-28 07:43:48 +0530

    multer configuration

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -1,8 +1,10 @@
 import mongoose, { Schema } from "mongoose";
 import jwt from "jsonwebtoken";
 import bcrypt from "bcrypt";
-const userSchema = new mongoose.Schema({
 
+// Define the schema for the user
+const userSchema = new mongoose.Schema({
+    // Username field: unique, lowercase, trimmed, and indexed for efficient querying
     username: {
         type: String,
         required: true,
@@ -11,6 +13,7 @@ const userSchema = new mongoose.Schema({
         trim: true,
         index: true
     },
+    // Email field: unique, lowercase, and trimmed
     email: {
         type: String,
         required: true,
@@ -18,73 +21,76 @@ const userSchema = new mongoose.Schema({
         lowercase: true,
         trim: true
     },
+    // Full name field: required and trimmed
     fullname: {
         type: String,
         required: true,
         trim: true,
         index: true
     },
+    // Avatar field: stores the URL of the user's avatar image (assumed to be from a cloud service)
     avatar: {
-        type: String, //using cloudnary service
+        type: String,
         required: true
     },
+    // Cover image field: stores the URL of the user's cover image
     coverImage: {
         type: String
-
     },
+    // Watch history field: stores an array of ObjectIds referencing video documents
     watchHistory: [{
         type: Schema.Types.ObjectId,
         ref: "Video"
     }],
+    // Password field: required with custom error message and will be hashed before saving
     password: {
         type: String,
-        required: [trur, 'Password is required']
+        required: [true, 'Password is required']
     },
-    refereshToken: {
+    // Refresh token field: stores the refresh token used for token refreshment
+    refreshToken: {
         type: String
     }
-}, { timestamps: true })
+}, { timestamps: true }); // Timestamps for createdAt and updatedAt fields
 
-
+// Pre-save hook to hash the password before saving
 userSchema.pre("save", async function(next) {
-
     if (!this.isModified("password")) {
         return next();
-
     }
+    // Hashing the password with bcrypt
+    this.password = await bcrypt.hash(this.password, 10);
+    next();
+});
 
-    this.password = bcrypt.hash(this.password, 10)
-    next()
-})
-
+// Method to check if the provided password is correct
 userSchema.methods.isPasswordCorrect = async function(password) {
-    return await bcrypt.compare(password, this.password)
-}
+    // Comparing the provided password with the hashed password
+    return await bcrypt.compare(password, this.password);
+};
 
-
-userSchema.methods.generateAcessToken = function() {
+// Method to generate an access token for the user
+userSchema.methods.generateAccessToken = function() {
+    // Generating JWT access token with user data and expiration
     return jwt.sign({
-            _id: this._id,
-            email: this.email,
-            username: this.username,
-            fullname: this.fullname
-        },
-        process.env.ACCESS_TOKEN_SECRET, {
-            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
-        }
+        _id: this._id,
+        email: this.email,
+        username: this.username,
+        fullname: this.fullname
+    }, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+    });
+};
 
-    )
-}
+// Method to generate a refresh token for the user
 userSchema.methods.generateRefreshToken = function() {
+    // Generating JWT refresh token with user ID and expiration
     return jwt.sign({
-            _id: this._id
+        _id: this._id
+    }, process.env.REFRESH_TOKEN_SECRET, {
+        expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+    });
+};
 
-        },
-        process.env.REFRESH_TOKEN_SECRET, {
-            expiresIn: process.env.REFRESH_TOKEN_EXPIRY
-        }
-
-    )
-}
-
-export const User = mongoose.model("User", userSchema)
+// Create a Mongoose model based on the schema
+export const User = mongoose.model("User", userSchema);
--- src/models/viedo.models.js
+++ src/models/viedo.models.js
@@ -1,54 +1,53 @@
 import mongoose, { Schema } from "mongoose";
 import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
 
-const videoSchema = new mongoose.Schema(
+// Define the schema for the video
+const videoSchema = new mongoose.Schema({
+    // Video file field: stores the URL or path of the video file
+    videoFile: {
+        type: String,
+        required: true
+    },
+    // Thumbnail field: stores the URL or path of the video thumbnail image
+    thumbNail: {
+        type: String,
+        required: true
+    },
+    // Title field: stores the title of the video
+    title: {
+        type: String,
+        required: true
+    },
+    // Description field: stores the description of the video
+    description: {
+        type: String,
+        required: true
+    },
+    // Duration field: stores the duration of the video (assumed to be retrieved from cloudinary)
+    duration: {
+        type: Number,
+        required: true
+    },
+    // Views field: stores the number of views of the video, defaults to 0
+    views: {
+        type: Number,
+        required: true,
+        default: 0
+    },
+    // isPublished field: indicates whether the video is published or not, defaults to true
+    isPublished: {
+        type: Boolean,
+        default: true
+    },
+    // Owner field: stores the ObjectId of the user who owns the video
+    owner: {
+        type: Schema.Types.ObjectId,
+        ref: "User" // Reference to the User model
+    }
+}, { timestamps: true }); // Timestamps for createdAt and updatedAt fields
 
-    {
-        videoFile: {
-            type: String,
-            required: true
+// Plugin for pagination using mongoose-aggregate-paginate-v2
+videoSchema.plugin(mongooseAggregatePaginate);
 
-        },
-        thumbNail: {
-            type: String,
-            required: true
-
-        },
-        title: {
-            type: String,
-            required: true
-        },
-        description: {
-            type: String,
-            required: true
-
-        },
-        duration: {
-            type: Number, //we will get this from cloudnary
-            required: true
-
-        },
-        views: {
-            type: Number,
-            required: true,
-            default: 0
-
-        },
-        isPublished: {
-            type: Boolean,
-            default: true
-        },
-        owner: {
-            type: Schema.Types.ObjectId,
-            ref: "User"
-        }
-
-
-
-
-
-    }, { timestamps: true })
-
-videoSchema.plugin(mongooseAggregatePaginate)
-
-export const Video = mongoose.model("Video", videoSchema)
+// Create a Mongoose model based on the schema
+export const Video = mongoose.model("Video", videoSchema);
